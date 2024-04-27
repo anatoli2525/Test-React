47cdbca5e4a8554e99ebe186d56fedafcd059e71
anatoli <johndoe@example.com>
2024-04-27 12:00:43 +0400

    first commit

---
+++ src/Hello/Hello.jsx
@@ -0,0 +1,6 @@
+import '../index.css'
+export default function Hello() {
+    return (
+       <h1 className="hello">HELLO</h1>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import Hello from "./Hello/Hello"
+
+const jsxElement =(
+  <Hello/>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(jsxElement)
